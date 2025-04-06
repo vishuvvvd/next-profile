21e5c69ed6033deab8cee2ba971061f101a5b889
vishal <vishu.vv.vd@gmail.com>
2025-04-06 23:51:53 +0530

    eslint rule update for async api calls

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,11 @@
 import Home from '@/components/home';
 import AppLayoutWrapper from '@/components/layout-wrapper';
-import { getUserMeData } from '@/services/main/me';
 
 export default async function HomePage() {
-  const data = await getUserMeData({ locale: 'en' });
 
   return (
     <AppLayoutWrapper>
-      <Home data={data} />
+      <Home />
     </AppLayoutWrapper>
   );
 }
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,11 @@
 import { Box } from '@mui/material';
 
+import { getUserMeData } from '@/services/main/me';
+
 import Profile from './profile';
-// import { getUserMeData } from '@/services/main/me';
 
-const Home = ({ data }: { data: { imageUrl: string, bio: string, } }) => {
-  // const data1 = await getUserMeData();
-
+const Home = async () => {
+  const data = await getUserMeData({});
   return (
     <Box>
       <Profile data={data} />
