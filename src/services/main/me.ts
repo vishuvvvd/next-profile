bb5c82e469a438886f99531f214c24299f7f86e8
vishal <vishu.vv.vd@gmail.com>
2025-04-06 15:50:10 +0530

    quotes

--- src/services/main/me.ts
+++ src/services/main/me.ts
@@ -1,5 +1,5 @@
-import { INTERNAL_API } from "@/constants/api";
-import API from "@/helpers/fetch";
+import { INTERNAL_API } from '@/constants/api';
+import API from '@/helpers/fetch';
 
 export const getUserMeData = async () => {
   const { data = {} } = await API(INTERNAL_API.ME, { noAuth: true });
