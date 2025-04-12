53f31281608933ea0609bc7a1c6fb50d9c646f94
vishal <vishu.vv.vd@gmail.com>
2025-04-12 23:35:46 +0530

    import order change

--- src/helpers/server-utils.ts
+++ src/helpers/server-utils.ts
@@ -1,19 +1,19 @@
-"use server";
+'use server';
 
-import { headers } from "next/headers";
-import { getLocale } from "next-intl/server";
+import { headers } from 'next/headers';
+import { getLocale } from 'next-intl/server';
 
-import { XREVALIDATE } from "@/constants";
-import { INTERNAL_NEXT_API_BASE } from "@/config";
+import { INTERNAL_NEXT_API_BASE } from '@/config';
+import { XREVALIDATE } from '@/constants';
 
 export async function getServerLocaleRevalidate() {
   const headersList = await headers();
   const locale = await getLocale();
   const revalidate = headersList.get(XREVALIDATE);
-  const forwardedProto = headersList.get("x-forwarded-proto");
+  const forwardedProto = headersList.get('x-forwarded-proto');
 
-  const protocol = forwardedProto || "http"; // fallback to 'http'
-  const host = headersList.get("x-forwarded-host") ?? headersList.get("host");
+  const protocol = forwardedProto || 'http'; // fallback to 'http'
+  const host = headersList.get('x-forwarded-host') ?? headersList.get('host');
   const hostUrl = INTERNAL_NEXT_API_BASE || `${protocol}://${host}`;
   return { locale, revalidate, hostUrl };
 }
