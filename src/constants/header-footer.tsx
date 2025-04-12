e6fae3c7fb340e468db44cf650c04dccfea41611
vishal <vishu.vv.vd@gmail.com>
2025-04-13 00:11:53 +0530

    social media updated

--- src/constants/header-footer.tsx
+++ src/constants/header-footer.tsx
@@ -2,7 +2,6 @@ import CallIcon from '@mui/icons-material/Call';
 import DescriptionIcon from '@mui/icons-material/Description';
 import EmailIcon from '@mui/icons-material/Email';
 import FacebookIcon from '@mui/icons-material/Facebook';
-import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import PersonIcon from '@mui/icons-material/Person';
@@ -16,11 +15,6 @@ import { CONTACT_EMAIL, CONTACT_NUMBER } from '.';
 
 export const socialMediaList = [
   {
-    name: 'github',
-    icon: <GitHubIcon />,
-    url: 'https://github.com'
-  },
-  {
     name: 'gmail',
     icon: <EmailIcon />,
     url: `mailto:${CONTACT_EMAIL}`
@@ -28,12 +22,12 @@ export const socialMediaList = [
   {
     name: 'facebook',
     icon: <FacebookIcon />,
-    url: 'https://www.facebook.com'
+    url: 'https://m.facebook.com/vishal.patil.241187/'
   },
   {
     name: 'instagram',
     icon: <InstagramIcon />,
-    url: 'https://www.instagram.com'
+    url: 'https://www.instagram.com/v.______________________.v/'
   },
   {
     name: 'whatsapp',
@@ -43,12 +37,12 @@ export const socialMediaList = [
   {
     name: 'linkedin',
     icon: <LinkedInIcon />,
-    url: 'https://www.linkedin.com'
+    url: 'https://www.linkedin.com/in/vishal-patil-278624144'
   },
   {
     name: 'twitter',
     icon: <TwitterIcon />,
-    url: 'https://twitter.com'
+    url: 'https://x.com/vishudpatil'
   },
 ];
 
